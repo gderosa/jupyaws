9809f9ee7c8930dd939bc5cad3f99eb05890a6f3
Guido De Rosa <gderosa@umail.ucc.ie>
2019-02-24 03:23:56 +0000

    initial

---
+++ example.variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {
+  default = "XXXXXXXXXXXX"
+}
+
+variable "aws_secret_key" {
+  default = "YYYYYYYYYYYYYYYYYYYYYYYYYYY"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "account_id" {
+  default = "1234567890"
+}
+
+variable "bucket_name" {
+  default = "unique-name-tf-lambdas"
+}
